3b799a595e78a8c02f02fde38824ac49b592be67
amurrai <andre.murrai@gmail.com>
2023-11-27 22:33:42 -0500

    add loop to format date and time of the result array

--- index.js
+++ index.js
@@ -31,5 +31,10 @@ nextISSTimesForMyLocation((error, passTimes) => {
     return console.log("It didn't work!", error);
   }
   // success, print out the deets!
-  console.log(passTimes);
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
 });
